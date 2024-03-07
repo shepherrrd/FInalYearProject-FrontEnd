d89a5fed4cf5d630d46912350922d3bed7f4136d
Ifeoluwa Ibukun-ojo <iibukunojo@gmail.com>
2024-03-07 07:49:36 +0100

    update

--- src/app/Hospital/manageData/page.tsx
+++ src/app/Hospital/manageData/page.tsx
@@ -1,9 +1,6 @@
-import HospitalDashboard from "@/layouts/clinic/managedataHospital/hospital.dashboard";
-
 export default function Dashboard() {
   return (
     <>
-      <HospitalDashboard />
     </>
   );
 }
